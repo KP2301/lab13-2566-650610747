922e41117eaee324be4f9b15a4b6475a2baf5c7e
Kittiwin Phannachet <kittiwinktb@gmail.com>
2023-08-07 15:37:47 +0700

    commit2.0

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -3,12 +3,13 @@
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
-export default function SearchResultPage({params}) {
-
+export default function SearchResultPage({ params }) {
   const searchInput = params.searchInput;
-  const processedSearchInput = searchInput.replaceAll("%20"," ")
+  const processedSearchInput = searchInput.replaceAll("%20", " ");
   const filteredMovies = movieDB.filter((movie) =>
-    movie.title.toLocaleLowerCase().includes(processedSearchInput.toLocaleLowerCase())
+    movie.title
+      .toLocaleLowerCase()
+      .includes(processedSearchInput.toLocaleLowerCase())
   );
 
   return (
@@ -16,15 +17,17 @@ export default function SearchResultPage({params}) {
       <p className="fw-bold fs-4 text-center my-0">
         Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found {filteredMovies.length} result(s)</p>
-      {filteredMovies.map((movie, i)=>(
-        <MovieRow 
+      <p className="fw-bold fs-4 text-center">
+        Found {filteredMovies.length} result(s)
+      </p>
+      {filteredMovies.map((movie, i) => (
+        <MovieRow
           key={movie.id}
-          id = {movie.id}
-          title = {movie.title}
-          detail = {movie.detail}
-          rating = {movie.rating}
-          number = {i+1}
+          id={movie.id}
+          title={movie.title}
+          detail={movie.detail}
+          rating={movie.rating}
+          number={i + 1}
         />
       ))}
     </div>
